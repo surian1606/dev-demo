24fc9636b4893471567d7ba29aa724703a9de0c8
Surian Syah <suriansy@yahoo.co.id>
2022-12-14 19:39:01 +0700

    updated code for multi demo

--- boto3/hard-coded-key.py
+++ boto3/hard-coded-key.py
@@ -1,13 +1,18 @@
 import boto3
 
-ACCESS_KEY='your-access-key-id-here'
-SECRET_KEY='your-secret-access-key-here'
+##Remove the comment for using hard-coded credentials
+#ACCESS_KEY='<your-access-key-id-here>'
+#SECRET_KEY='<your-secret-access-key-here>'
 
 s3 = boto3.client(
-    's3',
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY
+    's3'#,
+    #aws_access_key_id=ACCESS_KEY,
+    #aws_secret_access_key=SECRET_KEY
     )
+
+#To use custom profile, please use below snippet instead
+#s3 = boto3.session.Session(profile_name='developer').client('s3')
+
 response = s3.list_buckets()
 
 print('Existing buckets:')
